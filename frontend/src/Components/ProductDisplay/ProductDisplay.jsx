ed0aef0b2169f355b655078cfe606935343ff203
KetanKumar3 <ketanaryan2004@gmail.com>
2025-09-11 23:14:34 +0530

    feat:added some component

---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import star_icon from '../Assets/star_icon.png'
+import star_dull_icon from '../Assets/star_dull_icon.png'
+
+const ProductDisplay = () => {
+  return (
+    <div className='productdisplay'>
+        <div className='productdisplay-left'>
+            <div className='productdisplay-img-list'>
+                <img src={product.image} />
+                <img src={product.image} />
+                <img src={product.image} />
+                <img src={product.image} />
+            </div>
+            <div className="productdisplay-img">
+                <img className='productdisplay-main-img' src={product.image} />
+            </div>
+        </div>
+        <div className='productdisplay-right'>
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-star">
+               <img src={star_icon} />
+               <img src={star_icon} />
+               <img src={star_icon} />
+               <img src={star_icon} />
+               <img src={star_dull_icon} />
+               <p>(122)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className='productdisplay-right-description'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum iste, minus quidem beatae impedit sunt quaerat asperiores praesentium porro pariatur amet provident sed sit ullam inventore ad quas blanditiis maxime?
+            </div>
+            <div className='productdisplay-right-size'>
+                <h1>Select Size</h1>
+                <div className='productdisplay-right-size'>
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button>Add to Cart</button>
+            <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop-Top</p>
+            <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/loginsignup'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+       <div className='loginsignup-container'>
+         <h1>Signup</h1>
+         <div className='loginsignup-fields'>
+           <input type='text' placeholder='Your Name' />
+           <input type='email' placeholder='Email Address' />
+           <input type='password' placeholder='Password' />
+         </div>
+         <button>Continue</button>
+         <p className='loginsignup-login'>Already Have an account?<span>login here</span></p>
+         <div className="loginsignup-agree">
+          <input type='checkbox' name='' id='' />
+          <p>By Continuing, i agree to the terms of use & privacy policy.</p>
+         </div>
+       </div>
+    </div>
   )
 }
 
